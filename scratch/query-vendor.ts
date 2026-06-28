b3ea3b2f02ed0fbcf5e5ab183e1a718958c263ca
abhijith-bloombiz <abhijith.bloombiz@gmail.com>
2026-06-28 13:09:14 +0530

    fix

---
+++ scratch/query-vendor.ts
@@ -0,0 +1,19 @@
+import { connectToDatabase } from "../src/config/database";
+import { vendorService } from "../src/services/vendor.service";
+import mongoose from "mongoose";
+
+async function main() {
+  await connectToDatabase();
+  console.log("Connected to database. Querying vendor...");
+  const vendorId = "6a3d55a1ba76cce5e94a06ca";
+
+  const serviceResult = await vendorService.getVendorById(vendorId);
+  console.log("Service-retrieved vendor details:", serviceResult);
+
+  mongoose.connection.close();
+}
+
+main().catch((err) => {
+  console.error(err);
+  mongoose.connection.close();
+});
